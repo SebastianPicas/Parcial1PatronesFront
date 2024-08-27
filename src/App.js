2aecda6936feb1814c29159602f95d3acbeabdcf
Sebastian Piñeros <sebas.picas08@gmail.com>
2024-08-27 08:13:26 -0500

    Crear boton y caja de texto de registros

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        <h1>Registro de Datos</h1>
+      
+        <div>
+          <input
+            type="text"
+            placeholder="Ingrese un nuevo registro"
+          />
+          <button>Agregar Registro</button>
+        </div>
+      
+        <h2>Registros Almacenados</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Registro</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+          </tbody>
+        </table>
+      </body>
     </div>
   );
 }
